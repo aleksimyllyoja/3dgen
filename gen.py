585c9b165852086788a235bf18ae07296dfa9d02
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2021-10-09 18:10:56 +0300

    use bsplines

--- gen.py
+++ gen.py
@@ -3,12 +3,10 @@ import numpy as np
 import itertools
 from functools import reduce
 from scipy.special import comb
+from scipy.interpolate import splev
 from math import *
 from sdf import *
 from time import time
-
-def vec(*ps):
-    return np.array(ps, dtype=np.float64)
 
 def bernstein(i, n, t):
     return comb(n, i)*(t**(n-i))*(1-t)**i
@@ -25,6 +23,19 @@ def bezier(ps, n=5):
         list(zip(*reversed(ps)))
     )), axis=-1)
 
+def bspline(cv, n=100, degree=3):
+    count = cv.shape[0]
+    degree = np.clip(degree, 1, count-1)
+
+    kv = np.array(
+        [0]*degree+list(range(count-degree+1))+[count-degree]*degree,
+        dtype='int'
+    )
+
+    u = np.linspace(0, (count-degree), n)
+
+    return np.array(splev(u, (kv, cv.T, degree))).T
+
 def spherical_coordinates(v):
     r = np.linalg.norm(v)
     return (r, atan2(v[1], v[0]), acos(v[2]/r))
@@ -36,20 +47,24 @@ def cartesian_coordinates(r, theta, phi):
         r*cos(theta)
     ])
 
-def plot_lines_and_points(lines, points=None):
+def plot_lines_and_points(lines, points=[]):
     import matplotlib.pyplot as plt
 
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(projection='3d')
 
-    if points is not None:
-        xs, ys, zs = tuple(zip(*points))
-        ax.scatter(xs, ys, zs, marker='o')
+    for _points in points:
+        ax.scatter(
+            *tuple(zip(*_points)),
+            marker='o',
+            color=[np.random.uniform(0.1, 0.8) for x in range(3)]
+        )
 
     for line in lines:
-        color = [np.random.uniform(0, 0.6) for x in range(3)]
-        xs, ys, zs = tuple(zip(*line))
-        ax.plot(xs, ys, zs, color=color)
+        ax.plot(
+            *tuple(zip(*line)),
+            color=[np.random.uniform(0.1, 0.8) for x in range(3)]
+        )
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -84,26 +99,17 @@ def rotation_matrix(axis, theta):
 def split_count(p0, p1, split_length):
     return floor(np.linalg.norm(p1-p0)/split_length)
 
-def split_line(p0, p1, n):
-    return [p0+(p1-p0)/n*i for i in range(n+1)] if n>0 else [p0, p1]
+def split_line(p0, p1, step):
+    return [p0+(p1-p0)*k for k in np.arange(0, 1+step, step)]
 
 def circle_point(p, radius, lateral_angle, vertical_angle):
-    _pp = np.dot(
-        rotation_matrix(p, lateral_angle),
-        perpendicular_vector(p)
-    )
-    _pp = np.dot(
-        rotation_matrix(np.cross(p, _pp), vertical_angle),
-        _pp
-    )
+    _pp = np.dot(rotation_matrix(p, lateral_angle), perpendicular_vector(p))
+    _pp = np.dot(rotation_matrix(np.cross(p, _pp), vertical_angle), _pp)
 
     return _pp/(np.linalg.norm(_pp)/radius)
 
 def length(ps):
     return sum([np.linalg.norm(p1-p0) for p0, p1 in zip(ps, ps[1:])])
-
-def exponential_scale(x, min, max):
-    return exp(x*(log(max)-log(min))+log(min))
 
 def segmented_line(p0, p1, point_distance=0.5):
     return split_line(p0, p1, split_count(p0, p1, point_distance))
@@ -116,7 +122,7 @@ def with_accumulated_length_fractions(path):
 
 def vary_path(
     path,
-    f_radius=lambda x: np.uniform(0, 0.2),
+    f_radius=lambda x: np.random.uniform(0, 0.2),
     f_angle=lambda: np.random.uniform(0, 2*pi)
 ):
     return np.array([path[0]]+[
@@ -124,49 +130,50 @@ def vary_path(
         for l_acc, (p0, p1) in with_accumulated_length_fractions(path)[:-1]
     ]+[path[-1]])
 
-def curvy_path(
-    p0,
-    p1,
-    variation_division = 0.02,
+def line_to_curvy_path(
+    p0, p1,
+    division = 0.2,
     f_variation = lambda x: np.uniform(0, 0.2),
     f_angle = lambda: np.random.uniform(0, 2*pi),
     n = 200
 ):
-    return bezier(
-        vary_path(
-            segmented_line(p0, p1, point_distance=variation_division),
-            f_radius=f_variation,
-            f_angle=f_angle
-        ),
-        n=n
-    )
+    return bspline(vary_path(
+        split_line(p0, p1, division),
+        f_radius=f_variation,
+        f_angle=f_angle
+    ), n=n)
 
-def curvy_path_to_direction(
-    p0,
-    p1,
+def curvy_path_from_point(
+    point,
+    direction,
     length,
     lateral_angle,
     vertical_angle,
     variation_division,
     f_variation
 ):
-    return curvy_path(
-        p1, p1+circle_point(p1-p0, length, lateral_angle, vertical_angle),
-        variation_division=variation_division,
+    return line_to_curvy_path(
+        point, point+circle_point(direction, length, lateral_angle, vertical_angle),
+        division=variation_division,
         f_variation=f_variation
     )
 
 def constant(v):
     return lambda *args, **kwargs: v
 
+def _exponential_scale(x, min, max):
+    return exp(x*(log(max)-log(min))+log(min))
+
+def exponential_scale(min, max):
+    return lambda x: _exponential_scale(x, min, max)
+
 def reversed_exp_scale(min, max):
-    return lambda x: exponential_scale(1-x, min, max)
+    return lambda x: _exponential_scale(1-x, min, max)
 
 def sin_bell(min, max):
     return lambda x: sin(x*pi)*(max+min)-min
 
 def sdf_path(l, f_brush_size, f_k):
-
     total_length = length(l)
     cumulative_length = 0
     blob = sphere(f_brush_size(0), l[0])
@@ -220,15 +227,14 @@ def sdf_from_path_tree(
         ]
     )
 
-def tree_to_paths(tree):
-    def _flatten(t):
-        return [item for sublist in t for item in sublist]
+def flatten(t):
+    return [item for sublist in t for item in sublist]
 
+def tree_to_paths(tree):
     def _traverse(tree, paths=[]):
         if not tree.get('children'):
             return [tree.get('path')]
-        return [tree.get('path')]+_flatten([_traverse(c) for c in tree.get('children')])
-
+        return [tree.get('path')]+flatten([_traverse(c) for c in tree.get('children')])
 
     return list(_traverse(tree))
 
@@ -236,7 +242,8 @@ def setup():
     parser = argparse.ArgumentParser()
     parser.add_argument('--samples', default='2^16',
                         help='SDF render samples count')
-    parser.add_argument('--seed', type=int, default=int(time()),
+    parser.add_argument('--seed', type=int,
+                        default=np.random.randint(0, 2**32-1),
                         help='random seed')
     parser.add_argument('--save', action='store_true', default=False)
     parser.add_argument('--lineplot', action='store_true', default=False)
@@ -244,27 +251,22 @@ def setup():
 
     return args
 
-
-args = setup()
-
-print(f"RANDOM SEED {args.seed}")
-
-np.random.seed(args.seed)
-
-def gen_tree(p0, p1, **kwargs):
+def gen_tree(p0, p1, max_depth=2, **kwargs):
     def _gen_tree(_tree={}, **kwargs):
         _level = _tree.get('attr').get('level')
-        if _tree.get('attr').get('level') >= 2: return _tree
+        if _tree.get('attr').get('level') >= max_depth: return _tree
 
         _tree['children'] = [
             _gen_tree({
-                'path': curvy_path_to_direction(
-                    p0, p1,
+                'path':
+                 curvy_path_from_point(
+                    _p1,
+                    direction=_p1-_p0,
                     length=kwargs.get('f_branch_length')(l_acc),
                     lateral_angle=kwargs.get('f_lateral_angle')(l_acc),
                     vertical_angle=kwargs.get('f_vertical_angle')(l_acc),
                     variation_division=kwargs.get('f_variation_division')(l_acc),
-                    f_variation=kwargs.get('f_variation')(l_acc)
+                    f_variation=kwargs.get('f_variation')(l_acc),
                 ),
                 'attr': {
                     'level': _level+1,
@@ -273,7 +275,7 @@ def gen_tree(p0, p1, **kwargs):
                 'children': []
             }, **kwargs)
 
-            for l_acc, (p0, p1) in with_accumulated_length_fractions(_tree.get('path'))
+            for l_acc, (_p0, _p1) in with_accumulated_length_fractions(_tree.get('path'))
             for i in range(kwargs.get('f_branch_count')(l_acc, _level))
         ]
 
@@ -282,9 +284,9 @@ def gen_tree(p0, p1, **kwargs):
     return _gen_tree(
         **kwargs,
         _tree = {
-            'path': curvy_path(
-                np.array(p0), np.array(p1),
-                variation_division = kwargs.get('f_variation_division')(0),
+            'path': line_to_curvy_path(
+                p0, p1,
+                division = kwargs.get('f_variation_division')(0),
                 f_variation = kwargs.get('f_variation')(0)
             ),
             'attr': {
@@ -294,44 +296,57 @@ def gen_tree(p0, p1, **kwargs):
         }
     )
 
-path_tree = gen_tree(
-    (0, 0, -1), (0, 0, 1),
-    f_branch_count = lambda l_acc, level:
-        np.random.randint(1, 4) * (
-            0.20 < l_acc and
-            0.9 > l_acc and
-            bool(int(np.random.uniform(0, 1.02-level/40.0)))
-        ),
-    f_branch_length = reversed_exp_scale(0.4, 1.5),
-    f_variation = lambda l_acc: lambda x:
-        np.random.uniform(
-            0,
-            reversed_exp_scale(0.05, 0.2)(l_acc)
-        ),
-    f_variation_division = constant(0.05),
-    f_lateral_angle = lambda *args: np.random.uniform(0, 2*pi),
-    f_vertical_angle = lambda *args: np.random.uniform(pi, 2*pi)
-)
+args = setup()
 
-if args.lineplot: plot_lines_and_points(tree_to_paths(path_tree))
+print(f"RANDOM SEED {args.seed}")
 
-blob = sdf_from_path_tree(
-    path_tree,
-    f_brush_size = lambda level, **kwargs:
-        reversed_exp_scale(0.02, 0.2) if level==0 else
-        reversed_exp_scale(
-            0.01,
-            kwargs['parent_kwargs']['f_brush_size'](kwargs.get('l_acc_parent'))*0.6
-        )
-    ,
-    f_k_brush = lambda level, **kwargs: [
-        constant(0.01),
-        reversed_exp_scale(0.01, 0.05),
-        reversed_exp_scale(0.01, 0.05)
-    ][level],
-    f_k_paths = constant(0.02)
-)
+np.random.seed(args.seed)
 
-blob = blob & slab(z0=-1)
+p0 = np.array((0.0, 0.0, -1.0))
+p1 = np.array((0.0, 0.0, 1.0))
 
-if args.save: save_stl(blob, open_after_saving=True)
+if __name__ == "__main__":
+    path_tree = gen_tree(
+        p0, p1,
+        max_depth=2,
+        f_branch_count = lambda l_acc, level:
+            np.random.randint(1, 4 if level == 0 else 2) * (
+                0.1 < l_acc and
+                0.99 > l_acc and
+                1.0 < np.random.uniform(0, 1.02)
+            ),
+        f_branch_length = reversed_exp_scale(0.1, 0.8),
+        f_variation = lambda l_acc: lambda x:
+            np.random.uniform(
+                0,
+                reversed_exp_scale(0.05, 0.2)(l_acc)
+            ),
+        f_variation_division = exponential_scale(0.2, 0.4),
+        f_lateral_angle = lambda *args: np.random.uniform(0, 2*pi),
+        f_vertical_angle = lambda *args: np.random.uniform(pi, 2*pi)
+    )
+
+    if args.lineplot: plot_lines_and_points(tree_to_paths(path_tree))
+
+    blob = sdf_from_path_tree(
+        path_tree,
+
+        f_brush_size = lambda level, **kwargs:
+            reversed_exp_scale(0.02, 0.2) if level==0 else
+            reversed_exp_scale(
+                0.01,
+                kwargs['parent_kwargs']['f_brush_size'](kwargs.get('l_acc_parent'))*0.6
+            ),
+
+        f_k_brush = lambda level, **kwargs: [
+                constant(0.01),
+                reversed_exp_scale(0.01, 0.05),
+                reversed_exp_scale(0.01, 0.05)
+            ][level],
+
+        f_k_paths = constant(0.02)
+    )
+
+    blob = blob & slab(z0=-1)
+
+    if args.save: save_stl(blob, open_after_saving=True)
