fc8c68bc991d4b3e5225c6bb745135a5536fa6e7
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2021-10-23 19:14:08 +0300

    better bustos

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 * Branch direction
 
 ### Busto
+* Better forehead
+* Better head shape
+* Lips
+* Better eyes
+* Vary neck
+* Ears
--- busto.py
+++ busto.py
@@ -15,7 +15,7 @@ np_random_uniform = copy(np.random.uniform)
 def _uniform(a, b):
     v = np_random_uniform(a, b)
     fname = inspect.stack()[1].code_context[0].split("=")[0].strip()
-    print(f"{fname: <16} = {v:.4f}")
+    print(f"{fname: <32} {v:.4f}")
     return v
 
 np.random.uniform = _uniform
@@ -50,20 +50,20 @@ def forehead(face_width=1):
         (1, 0.5, 0),
     ]), n=100), f_brush_size=sin_bell(0.01, 0.1))
 
-def lips():
+def lips(opening=0.9, upper_lip_thickness=0.1, lower_lip_thickness=0.1):
     upper_lip = sdf_path(bspline(np.array([
         (-1, 0.5, 0),
         (-0.25, 0, 0.2),
         (0, 0, 0),
         (0.25, 0, 0.2),
         (1, 0.5, 0)
-    ]), n=50))
+    ]), n=50), f_brush_size=constant(upper_lip_thickness))
     
     lower_lip = sdf_path(bspline(np.array([
         (-1, 0.5, 0),
-        (0, 0, -0.5),
+        (0, 0, -opening),
         (1, 0.5, 0)
-    ]), n=50)).translate((0, 0, -0.1))
+    ]), n=50), f_brush_size=constant(lower_lip_thickness)).translate((0, 0, -0.1))
 
     upper_lip = upper_lip - ellipsoid((0.2, 0.2, 0.2)).translate((0, 0, 0.4)).k(0.2)
 
@@ -128,6 +128,7 @@ def grid(objs):
     return blob
 
 def busto():
+    max_head_width = 3
     jaw_width = np.random.uniform(0.25, 1.2)
     head_width = np.random.uniform(2, 3)
 
@@ -135,24 +136,30 @@ def busto():
     chin_depth = np.random.uniform(0, 0.3)
     chin_hardness = np.random.uniform(0.1, 0.45)
 
-    cheek_control_distance = np.random.uniform(1.7, 1.9)
-    cheek_control_height = np.random.uniform(-0.2, 0.2)
+    jaw_cut_height = np.random.uniform(-0.2, 0.2)
+    jaw_cut_hardness = np.random.uniform(0.2, 0.4)
 
     jaw = (
         rounded_cone(jaw_width, 1, 1.4-jaw_width) & slab(x0=0).k(0.7)
     ).translate((0, 0, 0.5*jaw_width)).scale((2, 1, 1)) & plane(-X).translate((1.5, 0, 0)).k(0.4)
 
-    jaw |= sdf_path(
+    chin = sdf_path(
         bspline(np.array([
-            (0.3, chin_width/2, 0),
+            (0.9, chin_width/2, 0),
+            (0.2, chin_width/2.2, 0),
             (0, 0, 0),
-            (0.3, -chin_width/2, 0),
-        ]), n=100), f_brush_size=sin_bell(0.01, 0.05)
-    ).translate((chin_depth, 0, 0.4*(1.2-jaw_width))).k(chin_hardness)
+            (0.2, -chin_width/2.2, 0),
+            (0.9, -chin_width/2, 0),
+        ]), n=100), f_brush_size=sin_bell(0.01, 0.02)
+    ).translate((chin_depth, 0, 0.4*(1.2-jaw_width)))
+    
+    jaw |= chin.k(chin_hardness)
 
     head_ball = sphere(1.25).translate((0.9, 0, 1.5)) & slab(x0=0).k(0.6)
 
     f_head = (head_ball | jaw.k(0.3)).rotate(pi/2, Z)
+
+    f_head = f_head & slab(z0=jaw_cut_height).k(jaw_cut_hardness)
 
     f_head -= eye_cavities((0.2, 0.1, 0.1)).translate((0, 0, 1.5)).k(0.4)
 
@@ -160,8 +167,11 @@ def busto():
 
     f_head = f_head - box((0.5, 10, 10), (-head_width/2.0, 0, 0)).k(0.5) - box((0.5, 10, 10), (head_width/2.0, 0, 0)).k(0.5)
 
-    f_head -= sphere(1).translate((cheek_control_distance, 0, cheek_control_height)).k(0.8)
-    f_head -= sphere(1).translate((-cheek_control_distance, 0, cheek_control_height)).k(0.8)
+    left_cheek_control = sphere(0.4).translate((-1.4-(max_head_width-head_width)/5.0, 0, 0.5))
+    right_cheek_control = sphere(0.4).translate((1.4-(max_head_width-head_width)/5.0, 0, 0.5))
+
+    f_head -= left_cheek_control.k(0.6)
+    f_head -= right_cheek_control.k(0.6)
 
     f_head |= eyes(
         pd=np.random.uniform(0.5, 0.8+head_width/10),
@@ -187,7 +197,16 @@ def busto():
     f_chest = sphere(1.3) & slab(z0=0, x0=-1, x1=1) & plane(-Y).translate((0, 0.6, 0))
     f_head |= f_chest.translate((0, 1.2, -1.5)).k(0.1)
 
-    f_head |= lips().scale(0.3).translate((0, -0.1, 0.75)).k(0.1)
+    lip_depth = np.random.uniform(0.1, 0.2)
+    lip_height = np.random.uniform(0.6, 0.8)
+
+    f_lips = lips(
+        opening = np.random.uniform(0, 1.2),
+        upper_lip_thickness = np.random.uniform(0.03, 0.2),
+        lower_lip_thickness = np.random.uniform(0.03, 0.2)
+    ).scale(0.3).translate((0, -lip_depth, lip_height))
+
+    f_head |= f_lips.k(0.1)
 
     return f_head
 
@@ -198,8 +217,8 @@ if __name__ == "__main__":
 
     np.random.seed(args.seed)
 
-    f = busto()
-    #f = grid([busto() for i in range(4)])
+    #f = busto()
+    f = grid([busto() for i in range(4)])
 
     print(f"SAMPLES {colored(args.samples, 'green')}")
     save_stl(
