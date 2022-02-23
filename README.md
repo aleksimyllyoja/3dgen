a4db45edc8ba50d4df51d91254f8933045e0db64
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2022-02-23 13:03:36 +0200

    asd

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # 3dgen
-
-
-## todo
-* Tests?
-
-### Plant
-* Branch direction
-
-### Busto
-* Better forehead
-* Better head shape
-* Lips
-* Better eyes
-* Vary neck
-* Ears
--- busto.py
+++ busto.py
@@ -12,10 +12,12 @@ from copy import copy
 
 np_random_uniform = copy(np.random.uniform)
 
+OVERWRITES = {}
+
 def _uniform(a, b):
-    v = np_random_uniform(a, b)
     fname = inspect.stack()[1].code_context[0].split("=")[0].strip()
-    print(f"{fname: <32} {v:.4f}")
+    v = OVERWRITES.get(fname, np_random_uniform(a, b))
+    print(f"{fname: <32} {v:.4f} [{a:.3f}, {b:.3f}]")
     return v
 
 np.random.uniform = _uniform
@@ -24,7 +26,7 @@ def eye(opening = pi/9, scale=0.15):
     f = sphere() & slab(z0=0)
     f2 = copy(f).rotate(-opening, X).translate((0, -0.2, 0))
     f = f.rotate(pi, X).rotate(opening, X)
-    f = f | f2 
+    f = f | f2
     f = f | sphere(0.8)
 
     return f.scale(scale)
@@ -35,19 +37,22 @@ def eyes(pd=0.5, scale=0.15, left_opening=pi/9, right_opening=pi/9):
 
     return eye_1 | eye_2
 
-def eye_cavities(size, pd=0.7):
-    eyec_1 = ellipsoid(size).translate((-pd/2, 0, 0)) & plane(-X).translate((-0.3, 0, 0)).k(0.2)
-    eyec_2 = ellipsoid(size).translate((pd/2, 0, 0)) & plane(X).translate((0.3, 0, 0)).k(0.2)
+def eye_cavities(size, cavity_distance=0.7):
+    eyec_1 = ellipsoid(size).translate((-cavity_distance/2, 0, 0))
+    eyec_1 &= plane(-X).translate((-cavity_distance/2, 0, 0)).k(0.2)
+
+    eyec_2 = ellipsoid(size).translate((cavity_distance/2, 0, 0))
+    eyec_2 &= plane(X).translate((cavity_distance/2, 0, 0)).k(0.2)
 
     return eyec_1 | eyec_2.k(0.2)
 
 def forehead(face_width=1):
     return sdf_path(bspline(np.array([
-        (-1, 0.5, 0),
+        (-1, 0.4, 0),
         (-0.5, 0, 0.2),
-        (0, 0, -0.2),
+        (0, 0.5, -0.2),
         (0.5, 0, 0.2),
-        (1, 0.5, 0),
+        (1, 0.4, 0),
     ]), n=100), f_brush_size=sin_bell(0.01, 0.1))
 
 def lips(opening=0.9, upper_lip_thickness=0.1, lower_lip_thickness=0.1):
@@ -58,7 +63,7 @@ def lips(opening=0.9, upper_lip_thickness=0.1, lower_lip_thickness=0.1):
         (0.25, 0, 0.2),
         (1, 0.5, 0)
     ]), n=50), f_brush_size=constant(upper_lip_thickness))
-    
+
     lower_lip = sdf_path(bspline(np.array([
         (-1, 0.5, 0),
         (0, 0, -opening),
@@ -70,50 +75,47 @@ def lips(opening=0.9, upper_lip_thickness=0.1, lower_lip_thickness=0.1):
     return upper_lip | lower_lip
 
 def nose(
-    tip_size=0.9, 
+    tip_size=0.9,
     nostril_size=0.2,
     nose_angle=pi/12,
     depth=0.4,
-    hook_strength=0.4,
+    hook_upper_strength=0.4,
+    hook_lower_strength=0.4,
     nose_width=0.5,
     subnasal_angle=pi/16,
     subnasal_cut=-0.4,
 ):
     n1 = sdf_path(bspline(np.array([
-        (0, 0, 4),
-        (0, 0, 3.5),
-        (0, -hook_strength, 2.5),
+        (0, 0.1-hook_upper_strength/10.0, 3),
+        (0, -hook_upper_strength, 2.5),
+        (0, -hook_lower_strength, 2),
         (0, 0, 0),
-    ]), n=50), f_brush_size=exponential_scale(tip_size*0.2, tip_size))
+    ]), n=50), f_brush_size=exponential_scale(tip_size*0.5, tip_size))
 
     n2 = sdf_path(bspline(np.array([
         (0, depth, 3),
         (0, depth, 0),
         (-nose_width/2, depth, 0.2),
-    ]), n=50), f_brush_size=exponential_scale(0.1, tip_size*nostril_size))
+    ]), n=50), f_brush_size=exponential_scale(0.2, nostril_size*tip_size))
 
     n3 = sdf_path(bspline(np.array([
         (0, depth, 3),
         (0, depth, 0),
         (nose_width/2, depth, 0.2),
-    ]), n=50), f_brush_size=exponential_scale(0.1, tip_size*nostril_size))
+    ]), n=50), f_brush_size=exponential_scale(0.2, nostril_size*tip_size))
 
-    f_nose = n1 | (n2 | n3).k(0.2)
+    filler = sdf_path(bspline(np.array([
+        (0, 0.2-hook_upper_strength/10.0, 3),
+        (0, -hook_upper_strength, 2.5),
+        (0, -hook_lower_strength, 2),
+        (0, 0.2, 0),
+    ]), n=50), f_brush_size=exponential_scale(tip_size*0.5, tip_size))
+
+    f_nose = (n1 | filler.k(0.2)) | (n2 | n3)
 
     f_nose &= plane(Z).translate((0, 0, subnasal_cut)).rotate(subnasal_angle, X).k(0.2)
 
     return f_nose.rotate(nose_angle, -X).scale(0.2)
-
-def setup():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--samples', default='2^16',
-                        help='SDF render samples count')
-    parser.add_argument('--seed', type=int,
-                        default=np.random.randint(0, 2**32-1),
-                        help='random seed')
-    args = parser.parse_args()
-
-    return args
 
 def grid(objs):
     blob = objs[0]
@@ -130,7 +132,7 @@ def grid(objs):
 def busto():
     max_head_width = 3
     jaw_width = np.random.uniform(0.25, 1.2)
-    head_width = np.random.uniform(2, 3)
+    head_width = np.random.uniform(2, max_head_width)
 
     chin_width = np.random.uniform(0.1, 0.6)
     chin_depth = np.random.uniform(0, 0.3)
@@ -141,7 +143,7 @@ def busto():
 
     jaw = (
         rounded_cone(jaw_width, 1, 1.4-jaw_width) & slab(x0=0).k(0.7)
-    ).translate((0, 0, 0.5*jaw_width)).scale((2, 1, 1)) & plane(-X).translate((1.5, 0, 0)).k(0.4)
+    ).translate((0, 0, 0.5*jaw_width)).scale((1.5, 1, 1)) & plane(-X).translate((1.5, 0, 0)).k(0.4)
 
     chin = sdf_path(
         bspline(np.array([
@@ -152,20 +154,36 @@ def busto():
             (0.9, -chin_width/2, 0),
         ]), n=100), f_brush_size=sin_bell(0.01, 0.02)
     ).translate((chin_depth, 0, 0.4*(1.2-jaw_width)))
-    
+
     jaw |= chin.k(chin_hardness)
 
-    head_ball = sphere(1.25).translate((0.9, 0, 1.5)) & slab(x0=0).k(0.6)
+    head_ball = sphere(1.25).translate((1.0, 0, 1.5)) & slab(x0=0).k(0.6)
 
     f_head = (head_ball | jaw.k(0.3)).rotate(pi/2, Z)
 
     f_head = f_head & slab(z0=jaw_cut_height).k(jaw_cut_hardness)
 
-    f_head -= eye_cavities((0.2, 0.1, 0.1)).translate((0, 0, 1.5)).k(0.4)
+    nose_tip_size = np.random.uniform(0.4, 1.5)
+    f_head |= nose(
+        tip_size=nose_tip_size,
+        nostril_size=np.random.uniform(0.2, 0.9),
+        nose_angle=np.random.uniform(pi/16, pi/10),
+        depth=np.random.uniform(0.1, 1.0),
+        hook_upper_strength=np.random.uniform(-0.5, 0.5),
+        hook_lower_strength=np.random.uniform(-1.0, 1.4),
+        nose_width=np.random.uniform(0.5, 1.7),
+        subnasal_angle=np.random.uniform(pi/16, pi/6),
+        subnasal_cut=np.random.uniform(-0.6, 0.8)
+    ).translate((0, -0.2, 1.0)).k(0.1)
 
-    f_head |= forehead().scale(0.6).translate((0, 0.0, 1.7)).k(0.2)
+    f_head -= eye_cavities(
+        (0.6, 0.2, 0.2),
+        cavity_distance = nose_tip_size*0.4
+    ).translate((0, 0, 1.5)).k(0.2)
 
-    f_head = f_head - box((0.5, 10, 10), (-head_width/2.0, 0, 0)).k(0.5) - box((0.5, 10, 10), (head_width/2.0, 0, 0)).k(0.5)
+    f_head |= forehead().scale(0.75).translate((0, 0.0, 1.7)).k(0.2)
+
+    f_head = f_head - box((0.5, 10, 10), (-head_width/2.0, 0, 0)).k(0.4) - box((0.5, 10, 10), (head_width/2.0, 0, 0)).k(0.4)
 
     left_cheek_control = sphere(0.4).translate((-1.4-(max_head_width-head_width)/5.0, 0, 0.5))
     right_cheek_control = sphere(0.4).translate((1.4-(max_head_width-head_width)/5.0, 0, 0.5))
@@ -174,30 +192,13 @@ def busto():
     f_head -= right_cheek_control.k(0.6)
 
     f_head |= eyes(
-        pd=np.random.uniform(0.5, 0.8+head_width/10),
+        pd=np.random.uniform(0.6, 0.8+head_width/7.5),
         left_opening=np.random.uniform(pi/14, pi/4),
         right_opening=np.random.uniform(pi/14, pi/4),
         scale=np.random.uniform(0.15, 0.3)
-    ).translate((0, 0.1, 1.4)).k(0.1)
-    
-    f_head |= nose(
-        tip_size=np.random.uniform(0.4, 1.5),
-        nostril_size=np.random.uniform(0.2, 0.9),
-        nose_angle=np.random.uniform(pi/16, pi/10),
-        depth=np.random.uniform(0.3, 1.0),
-        hook_strength=np.random.uniform(-1.0, 1.6),
-        nose_width=np.random.uniform(0.5, 1.7),
-        subnasal_angle=np.random.uniform(pi/16, pi/6),
-        subnasal_cut=np.random.uniform(-0.8, 0.4)
-    ).translate((0, -0.2, 1.0)).k(0.1)
+    ).translate((0, 0.25, 1.4)).k(0.1)
 
-    # neck
-    f_head |= rounded_cone(0.5, 0.4, 1.2).translate((0, 1, -0.4)).k(0.2)
-
-    f_chest = sphere(1.3) & slab(z0=0, x0=-1, x1=1) & plane(-Y).translate((0, 0.6, 0))
-    f_head |= f_chest.translate((0, 1.2, -1.5)).k(0.1)
-
-    lip_depth = np.random.uniform(0.1, 0.2)
+    lip_depth = np.random.uniform(0.05, 0.1)
     lip_height = np.random.uniform(0.6, 0.8)
 
     f_lips = lips(
@@ -208,18 +209,42 @@ def busto():
 
     f_head |= f_lips.k(0.1)
 
-    return f_head
+    # arg no rotations
+    head_tilt = np.random.uniform(-pi/13, pi/13)
+    f_head = f_head.rotate(head_tilt, X)
+    #f_head = f_head.rotate(-pi/15, Y)
+    f_head = f_head.rotate(pi/15, Z)
+
+    # neck
+    f_neck = rounded_cone(0.7, 0.6, 1.2).translate((0, 1.4, -0.9)).rotate(pi/9, X)
+
+    f_chest = (
+        sphere(1.3) &
+        slab(z0=0, x0=-1, x1=1) &
+        plane(-Y).translate((0, 0.6, 0))
+    ).translate((0, 1.9, -1.5)).k(0.1)
+
+    f_chest |= f_neck.k(0.2)
+
+    return f_head | f_chest.k(0.2)
 
 if __name__ == "__main__":
-    args = setup()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--samples', default='2^16',
+                        help='SDF render samples count')
+    parser.add_argument('--seed', type=int,
+                        default=np.random.randint(0, 2**32-1),
+                        help='random seed')
+    parser.add_argument("-o", metavar="KEY=VALUE", nargs='+')
+    args = parser.parse_args()
 
-    print(f"RANDOM SEED {colored(args.seed, 'green')}")
-
+    OVERWRITES = dict((s.split('=')[0], float(s.split("=")[1])) for s in args.o or [])
     np.random.seed(args.seed)
 
-    #f = busto()
-    f = grid([busto() for i in range(4)])
+    f = busto()
+    #f = grid([busto() for i in range(4)])
 
+    print(f"RANDOM SEED {colored(args.seed, 'green')}")
     print(f"SAMPLES {colored(args.samples, 'green')}")
     save_stl(
         f,
